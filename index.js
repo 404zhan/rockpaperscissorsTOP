271c7c5600d4afd5b04637f64696ffc337022cba
Alamgir-09 <onedirection123124@gmail.com>
2025-06-17 10:57:27 +0530

    added the buttons for choices, and displayed score on the webpage instead of it being in the console

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 
-
-const b1 = document.getElementById("button1");
-const b2 = document.getElementById("button2");
-const b3 = document.getElementById("button3");
-b1.addEventListener('click', playRound());
-b2.addEventListener('click', playRound);
-b3.addEventListener('click', playRound);
-
-
-
-
 var playGame=function(){
     let humanScore=0;
     let computerScore=0;
+
+     const b1 = document.getElementById("button1");
+     const b2 = document.getElementById("button2");
+     const b3 = document.getElementById("button3");
+     b1.addEventListener("click", ()=>{
+        const playerSelection = b1.textContent;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+     });
+     b2.addEventListener("click", ()=>{
+        const playerSelection = b2.textContent;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+     });
+     b3.addEventListener("click", ()=>{
+        const playerSelection = b3.textContent;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+     });
+
+     const resPara = document.querySelector('.result1');
+     const scorePara = document.querySelector('.result2');
+     const choicePara = document.querySelector('.result3');
     function getComputerChoice(){
         let num = Math.floor(Math.random()*10);
         if(num>=0 && num<3){
@@ -25,35 +37,35 @@ var playGame=function(){
             return "Scissors";
         }
     }
-    function getHumanChoice(){
-        let userInput = prompt("Choose! ");
-        return userInput;
-    }
-const playRound = function playRound(humanChoice, computerChoice){
+    const playRound = function playRound(humanChoice, computerChoice){
         let hc=humanChoice.toLowerCase();
         let cc=computerChoice.toLowerCase();
-        console.log("Your Choice: "+hc+" ; "+"Computer's Choice: "+cc);
+        choicePara.textContent = "Your Choice: "+hc.toUpperCase()+" ; "+"Computer's Choice: "+cc.toUpperCase();
         if(hc===cc){
-         console.log("Who cares its a draw!");
+         resPara.textContent = `The ongoing score is Computer's: ${computerScore} Your: ${humanScore}`;
+         scorePara.textContent = "Who cares its a draw!";
         }
         else if((hc=="rock"&&cc=="paper")||(hc=="paper"&&cc=="scissors")||(hc=="scissors"&&cc=="rock")){
          computerScore++;
-         console.log("You lose!");
+         resPara.textContent = `The ongoing score is Computer's: ${computerScore} Your: ${humanScore}`;
+         scorePara.textContent = "You lose!";
         }
         else {
          humanScore++;
-         console.log("You Won!");
+         resPara.textContent = `The ongoing score is Computer's: ${computerScore} Your: ${humanScore}`;
+         scorePara.textContent = "You Won!";
         }
      }
      
-     if(humanScore>computerScore){
+
+    if(humanScore>computerScore){
         return `You've won the round by ${humanScore-computerScore} point!`;
      }
      else if(humanScore<computerScore){
         return `You've lost the round by ${computerScore-humanScore} point.`;
      }
      else{
-        return `The is a draw, Computer's Score: ${computerScore} Your Score: ${humanScore}`;
+        return `This is a draw, Computer's Score: ${computerScore} Your Score: ${humanScore}`;
      }
 }
 
